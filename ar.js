4c8753d2d204bc104e7df30098e41fd70851e235
“[firstname <[gmugsk@gmail.com]>
2024-06-16 11:55:19 +0500

    assignment

---
+++ ar.js
@@ -0,0 +1,14 @@
+"use strict";
+/*Question 17: Shrinking Guest List: Unfortunately, your new table won’t arrive in time, and you
+can only invite two guests.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Unfortunately, I can only invite two people for dinner.");
+// while (guests.length > 2) {
+// let removedGuest = guests.pop();
+// console.log(`Sorry, ${removedGuest}, I can't invite you to dinner.`);
+// }
+// guests.forEach(guest => {
+// console.log(`Dear ${guest}, you're still invited to dinner.`);
+// });
+// guests.splice(0, guests.length);
+// console.log(guests); // Shows an empty list
---
+++ main.ts
@@ -0,0 +1,42 @@
+/*Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, 
+and you have space for only two guests.
+• Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite
+ only two people for dinner.
+
+• Remove guests from your list one at a time until only two names remain in your list. Each time you pop a
+ name from your list, print a message to that person letting them know you’re sorry you can’t invite them 
+ to dinner.
+
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+
+• Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually 
+have an empty list at the end of your program.*/
+
+
+var guestlistArray: string []= ["Abdul Rehman", "Saifur Rehman", "Hamood ur Rehman","Ebad ur Rehman"];
+//  console.log(guestlistArray);
+
+
+//  console.log (`We have 3 more friends to invite on dinner/n`)
+//  guestlistArray.unshift("Mudassir");
+//  console.log(guestlistArray);
+
+//  myFriends.splice(2, 0, "Muhammad");
+//  console.log(myFriends);
+//  myFriends.push("Qasim");
+//  console.log(myFriends);
+
+for(let i = 0; i < guestlistArray.length; i++){
+    console.log(`Mr. ${guestlistArray[i]} You are invited to my dinner`);
+}
+console.log(`We have 3 more friends to invite on dinner/n`)
+// sorry we can not have big table , only 2 persons are invited.
+
+while(guestlistArray.length) {
+   let remove_guest=guestlistArray.pop();
+    console.log(`Sorry Mr.${remove_guest} You are not invited to dinner.`);
+for (let i=0; i < guestlistArray.length; i++) {
+       console.log (`Dear. ${guestlistArray[i]} You are still invited.Thank you!`)
+       guestlistArray.splice(0, 2)}}
+       guestlistArray.pop();
+ console.log(guestlistArray);
